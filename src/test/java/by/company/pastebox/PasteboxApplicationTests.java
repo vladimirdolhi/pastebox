e9ec7e2222c94c546fb779f97c516154e297e4cc
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2023-02-10 21:30:58 +0300

    Initial commit

---
+++ src/test/java/by/company/pastebox/PasteboxApplicationTests.java
@@ -0,0 +1,13 @@
+package by.company.pastebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
